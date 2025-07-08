25b1b1afef2c1c2a32b3e5910775d2d919b0dcad
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-07-08 16:14:54 -0400

    ffufufufu

--- api/server.js
+++ api/server.js
@@ -1,6 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('clear; echo explosion');
-}).listen(3000);
+  fs.readFile('script.sh', 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('Error reading file');
+      return;
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(data);
+  });
+})
--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+clear
 echo "yu hav ben theee hacketrf give me ya mony now!!11!!"
