1c42a0643e861d201e259930b796422943059b66
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-07-08 16:19:55 -0400

    ffufufufu

--- api/server.js
+++ api/server.js
@@ -4,8 +4,8 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.end(`#!/bin/bash
 clear
-for i in {1..500}; do
-  echo "Deleting files $i $(head -c 10 /dev/random | base64)"
+for i in {1..50}; do
+  echo "Stealing $i $(head -c 10 /dev/random | base64)"
 done
 clear
 echo "YOU ARE HAKCKDED AHAHAHHAHAHSHDAHHHAHHAHHAHHHAHHAHAAH"
