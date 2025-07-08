62e03cb25956ca3de7cd8eaaabfe0600fc09d5e5
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-07-08 16:08:53 -0400

    ffufufufu

--- api/server.js
+++ api/server.js
@@ -2,5 +2,5 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('clear; echo sup yo');
+  res.end('clear; echo explosion');
 }).listen(3000);
