fb5d55ce42adca1ee92c3ccb03ae5f7412eb83ea
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-07-08 15:45:12 -0400

    ffufufufu

--- api/server.js
+++ api/server.js
@@ -1,19 +1,21 @@
+const http = require('http');
 const { exec } = require('child_process');
 
-// Inside your HTTP handler:
-exec('echo hello world', (error, stdout, stderr) => {
-  console.log('exec callback triggered');
-  if (error) {
-    console.log('Error:', error);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    return res.end(`error\n`);
-  }
-  if (stderr) {
-    console.log('Stderr:', stderr);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    return res.end(`error\n`);
-  }
-  console.log('Stdout:', stdout);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(stdout);
-});
+http.createServer((req, res) => {
+  exec('echo hi', (error, stdout, stderr) => {
+    if (error) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(`Error: ${error.message}\n`);
+      return;
+    }
+    if (stderr) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(`stderr: ${stderr}\n`);
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(stdout);
+  });
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
