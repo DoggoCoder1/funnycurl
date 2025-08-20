c788467fbe4879e5206ada646944506ab1820e5b
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:22:48 -0400

    a

--- api/log.js
+++ api/log.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
     console.log('Received data:', data);
-    res.status(200).send('OK');  // simple acknowledgment
+    res.status(200).send(`${data}`);  // simple acknowledgment
   } else {
     res.status(405).send('Method Not Allowed');
   }
