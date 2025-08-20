8fa495ce0dba0ded7e6515bcda6bb808e898bbba
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:15:45 -0400

    a

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 export default function handler(req, res) {
   const userAgent = req.headers['user-agent'] || 'Unknown';
   res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send(`Your user agent is: ${userAgent}`);
+  const version = "1.0";
+  const code = 
+  `#!/bin/bash
+  echo "WowClicker Installer"
+  echo "Version: ${ver}"
+  
+  
+  `;
+  res.status(200).send(``);
 }
