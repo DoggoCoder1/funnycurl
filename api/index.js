c25ac2988a1ab5a2af14eb8ac6ba1611363921f8
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:50:46 -0400

    a

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain');
 
-  const code = `
-  #!/bin/bash
-  GREEN='\\033[0;32m'
-  NC='\\033[0m'  # No Color
-  ok() {
-  printf "[$GREENOK$NC] $1"
-  }
-  echo "WowClicker Installer VERSION 1,0"
-  USER_AGENT=$(uname)
-  if [ "$(uname)" = "Darwin" ]; then
-  ok "Detected MacOS."
-  fi
+  const code = `#!/bin/bash
+GREEN='\\033[0;32m'
+NC='\\033[0m'
+
+ok() {
+    printf "${GREEN}[OK]${NC} %s\\n" "$1"
+}
+
+echo "WowClicker Installer VERSION 1.0"
+
+USER_AGENT=$(uname)
+if [ "$USER_AGENT" = "Darwin" ]; then
+    ok "Detected macOS."
+fi
 `;
 
   res.status(200).send(code);
