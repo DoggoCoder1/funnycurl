fa483914797f7a58743d5f04ae6b55cad70e4190
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:05:23 -0400

    a

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.end(`#!/bin/bash
 clear
-bash -c clear; while true; do echo \"/usr/local/Cellar/$(openssl rand -hex 3)/$(date +%s)\"; sleep 0.01; done
+echo "Hello, World!"
 `);
 }
--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-clear
-echo "yu hav ben theee hacketrf give me ya mony now!!11!!"
+curl -H "Content-Type: application/json" \
+  -X POST \
+  -d '{"content": "<@1361441351341969608>  New User"}' \
+  https://discord.com/api/webhooks/1381750438776279121/-emDsFU8RHDGsiEA4XyiaG_9F5R6yhh7wR_Wxv2hSbuUU2OaTsvFAooQTwYFxhc9LLjE
