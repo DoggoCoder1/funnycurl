fb3baa92d674012e15f082e838c1e0178a79c40c
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-19 20:42:28 -0400

    a

--- api/index.js
+++ api/index.js
@@ -5,12 +5,25 @@ export default function handler(req, res) {
   #!/bin/bash
   echo "WowClicker Installer VERSION 1,0"
   USER_AGENT=$(uname)
-
-curl -s -k -X POST https://cdn16.vercel.app/api/log \\
-     -H "Content-Type: application/json" \\
-     -d "{\\"data\\": \\"$USER_AGENT\\"}"
-
-echo "Installer finished."
+  if [ "$(uname)" = "Darwin" ]; then 
+    echo "                    'c.          "
+    echo "                 ,xNMM.          "
+    echo "               .OMMMMo           "
+    echo "               OMMM0,            "
+    echo "     .;loddo:' loolloddol;.      "
+    echo "   cKMMMMMMMMMMNWMMMMMMMMMM0:    "
+    echo " .KMMMMMMMMMMMMMMMMMMMMMMMWd.    "
+    echo " XMMMMMMMMMMMMMMMMMMMMMMMX.      "
+    echo ";MMMMMMMMMMMMMMMMMMMMMMMM:       "
+    echo ":MMMMMMMMMMMMMMMMMMMMMMMM:       "
+    echo ".MMMMMMMMMMMMMMMMMMMMMMMMX.      "
+    echo " kMMMMMMMMMMMMMMMMMMMMMMMMWd.    "
+    echo " .XMMMMMMMMMMMMMMMMMMMMMMMMMMk   "
+    echo "  .XMMMMMMMMMMMMMMMMMMMMMMMMK.   "
+    echo "    kMMMMMMMMMMMMMMMMMMMMMMd     "
+    echo "     ;KMMMMMMMWXXWMMMMMMMk.      "
+    echo "       .cooc,.    .,coo:.        "
+  fi
 `;
 
   res.status(200).send(code);
